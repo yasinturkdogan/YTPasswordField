a983108ed5f642dce8d4aaff0f3f35103db812bb
yasinturkdogan <yasinturkdogan@gmail.com>
2017-01-02 16:30:42 +0200

    Update YTPasswordField.h

--- YTPasswordField/YTPasswordField.h
+++ YTPasswordField/YTPasswordField.h
@@ -2,8 +2,8 @@
 //  YTPasswordField.h
 //  YTPasswordField
 //
-//  Created by Rick Dangerous on 1/2/17.
-//  Copyright © 2017 incirmedya. All rights reserved.
+//  Created by Yasin Turkdogan on 1/2/17.
+//  Copyright © 2017 Turkdogan. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
