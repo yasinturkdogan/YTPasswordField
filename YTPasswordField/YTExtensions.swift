a9e6f1559c0297955d9056c995f7d968e026673c
yasinturkdogan <yasinturkdogan@gmail.com>
2017-01-02 16:30:24 +0200

    Update YTExtensions.swift

--- YTPasswordField/YTExtensions.swift
+++ YTPasswordField/YTExtensions.swift
@@ -2,8 +2,8 @@
 //  Extensions.swift
 //  PasswordField
 //
-//  Created by Rick Dangerous on 1/2/17.
-//  Copyright © 2017 incirmedya. All rights reserved.
+//  Created by Yasin Turkdogan on 1/2/17.
+//  Copyright © 2017 Turkdogan. All rights reserved.
 //
 
 import Foundation
